cc97f93f5f39bd917b64f9f25afb0a97008e30b0
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-15 23:22:26 +0900

    20210815

--- app.js
+++ app.js
@@ -13,8 +13,21 @@ console.dir(h1);
 
 
 function handleh2 () {
-    h1.innerText = "아닌뒈용~?"
+   if(h1.innerText === "저는 사람입니다") {
+       h1.innerText = "아뉜데용~?"
+   } else {
+       h1.innerText = "아뉘라구요 ㅋㅋ"
+   }
 };
 h1.addEventListener("mousewheel", handleh2) ;
 
 
+
+const h4 = document.querySelector(".I")
+console.log(h4);
+
+function hadleMouseLeave() {
+    h4.innerText = "ㅇㅈ~" ;
+};
+
+h4.addEventListener("mouseleave", hadleMouseLeave);
