bc11fadbf87159bc187141f1466fc38d41348d13
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-15 23:14:55 +0900

    20210815

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+const button = document.querySelector(".hello");
+console.log(button);
 
-const h2 =document.querySelector("h2")
+function handleClick(){
+    button.style.color ="aqua";
+}
+
+button.addEventListener("click", handleClick);
 
 
-function handleTitleClick2() {
-if(h2.innerText === "안녕하세요") {
-    h2.innerText = "안녕히 가세요!"
-} else {
-    h2.innerText = "안녕하세요!"
+const h1 =document.querySelector("h1");
+console.dir(h1);
+
+
+function handleh2 () {
+    h1.innerText = "아닌뒈용~?"
 };
-};
+h1.addEventListener("mousewheel", handleh2) ;
 
-h2.addEventListener("click", handleTitleClick2);
+
