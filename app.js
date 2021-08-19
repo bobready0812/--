d8132604410eb43a06d91b8cdf5941db32115d3d
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-19 22:13:22 +0900

    20210819

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
 function onLoginSubmit(aa) {
     aa.preventDefault();
    const username = loginInput.value;
    loginForm.classList.add ("hidden");
-   greeting.innerText =`Hello, ${username}`
-   greeting.classList.remove("hidden");
    localStorage.setItem("username", username);
+   paintGreetings(username);
+  
 }
 
-function handleClicked(event) {
-    event.preventDefault();
+function paintGreetings(username) {
+    greeting.innerText =`Hello, ${username}`;
+    greeting.classList.remove("hidden");
+}
+
+
+const savedUsername = localStorage.getItem("username");
+
+if(savedUsername === null) {
+loginForm.classList.remove("hidden");
+loginForm.addEventListener("submit", onLoginSubmit);
+
+} else {
+    paintGreetings(savedUsername);
     
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleClicked);
-
