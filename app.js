12ac2f26949ce42102c1748d169fee4cbe3fa282
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-19 20:35:28 +0900

    20210819

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
 function onLoginSubmit(aa) {
     aa.preventDefault();
-    console.log(loginInput.value);
+   const username = loginInput.value;
+   loginForm.classList.add ("hidden");
+   greeting.innerText =`Hello, ${username}`
+   greeting.classList.remove("hidden");
+   localStorage.setItem("username", username);
 }
 
 function handleClicked(event) {
     event.preventDefault();
+    
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleClicked);
+
