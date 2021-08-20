5df19ac9bea912a91fc0e58c3f0c11192580d486
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-20 21:54:21 +0900

    20210820

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const change = document.getElementById("change");
 
-function onLoginSubmit(aa) {
-    aa.preventDefault();
-   const username = loginInput.value;
-   loginForm.classList.add ("hidden");
-   localStorage.setItem("username", username);
-   paintGreetings(username);
-  
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+];
+
+function colorSelector() {
+    return colors[Math.floor(Math.random() * colors.length)];
 }
 
-function paintGreetings(username) {
-    greeting.innerText =`Hello, ${username}`;
-    greeting.classList.remove("hidden");
+function randomColor() {
+    const color1 = colorSelector();
+    const color2 = colorSelector();
+    document.body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+
 }
 
-
-const savedUsername = localStorage.getItem("username");
-
-if(savedUsername === null) {
-loginForm.classList.remove("hidden");
-loginForm.addEventListener("submit", onLoginSubmit);
-
-} else {
-    paintGreetings(savedUsername);
-    
-}
+change.addEventListener("click", randomColor);
