1162620bc26ac4f0b94eed383620cd71fb5a9f00
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-29 00:18:31 +0900

    20210829

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
-const change = document.getElementById("change");
+const button = document.querySelector("button");
+let count = 1;
 
-const colors = [
-    "#ef5777",
-    "#575fcf",
-    "#4bcffa",
-    "#34e7e4",
-    "#0be881",
-    "#f53b57",
-    "#3c40c6",
-    "#0fbcf9",
-    "#00d8d6",
-    "#05c46b",
-    "#ffc048",
-    "#ffdd59",
-    "#ff5e57",
-    "#d2dae2",
-    "#485460",
-    "#ffa801",
-    "#ffd32a",
-    "#ff3f34"
-];
-
-function colorSelector() {
-    return colors[Math.floor(Math.random() * colors.length)];
+function addclick () {
+    let remain = count % 2;
+    
+    if(remain = 0) {
+        button.innerText="hello!";
+    } else {
+        button.innerText="not~!";
+    }
+    
+    count++
+   
 }
 
-function randomColor() {
-    const color1 = colorSelector();
-    const color2 = colorSelector();
-    document.body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
 
-}
 
-change.addEventListener("click", randomColor);
+
+button.addEventListener("click", addclick);
