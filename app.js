73ec3cea8ebf4f060cf44f358c757ac1598be3e2
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-29 23:02:53 +0900

    <!DOCTYPE html> <html lang="en">     <head>         <meta charset="UTF-8">         <meta http-equiv="X-UA-Compatible" content="IE=edge">         <meta name="viewport" content="width=device-width, initial-scale=1.0">         <link rel="shortcut icon" type="image/x-icon" href="/src/asset/img/logo2.png">         <link rel="stylesheet" href="src/css/style.css">         <title>BlaBla</title>     </head>     <body>         <div class="all">             <div class="main1">
                </div>
                <div class="main2">
                    <button class="chat-box1">
                        <a></a>
                    </button>
                    <button class="chat-box2"></button>
                    <button class="chat-box3"></button>
                </div>
                <div class="container">
                    <div id="box1" class="box" style="background: cadetblue;">
                    </div>
                    <h1 id="btn1" class="hidden">✖</h1>
                    <div id="box2" class="box" style="background: rgb(113, 113, 255);">
                    </div>
                    <h1 id="btn2" class="hidden">✖</h1>
                </div>
            </div>
            <script src="src/js/background.js"></script>
            <script src="src/js/chat-box.js"></script>
        </body>
    </html>

--- app.js
+++ app.js
@@ -1,20 +1,98 @@
-const button = document.querySelector("button");
-let count = 1;
+const chat_box1 = document.querySelector(".chat-box1");
+const chat_box2 = document.querySelector(".chat-box2");
+const chat_box3 = document.querySelector(".chat-box3");
+const a = chat_box1.querySelector("a");
+let count = 0;
+let count2 ;
+let count3 ;
+let count4 ;
 
-function addclick () {
-    let remain = count % 2;
-    
-    if(remain == 0) {
-        button.innerText="hello!";
-    } else {
-        button.innerText="not~!";
-    }
-    
-    count++
-   
+
+function changeColor1 () {
+    chat_box1.style.background = "orange";
+}
+function changeColor2 () { 
+    chat_box2.style.background = "skyblue"
+}
+function changeColor3 () {
+    chat_box3.style.background = "palegreen";
 }
 
 
 
+function innerText1 () {
+    let remain = count % 2;
+    
+    if(remain == 0) {
+        chat_box1.innerText = "Hello!";
+        chat_box1.style.background = "orange";
+    } else {
+        chat_box1.innerText ="";
+    }
+    count++;
+    count2++
 
-button.addEventListener("click", addclick);
+}
+function innerText2 () {
+    let remain = count % 2;
+    
+    if(remain == 0) {
+        chat_box2.innerText = "I'm";
+        
+    } else {
+        chat_box2.innerText ="";
+    }
+    count++;
+    count3++
+    
+}
+function innerText3 () {
+   
+    let remain = count % 2;
+    if(remain == 0) {
+        chat_box3.innerText = "Daehyeon!";
+        
+    } else {
+        chat_box3.innerText ="";
+    }
+    count++;
+    count4++
+}
+
+function changeColor4 () { 
+    if(count2 == null ) {
+    chat_box1.style.background = "white";
+    } else {
+        chat_box1.style.background = "orange";
+    }
+}
+function changeColor5 () {
+    if(count3 == null ) {
+        chat_box2.style.background = "white";
+        } else {
+            chat_box2.style.background = "skyblue";
+        }
+}
+function changeColor6 () {
+    if(count4 == null ) {
+        chat_box3.style.background = "white";
+        } else {
+            chat_box3.style.background = "palegreen";
+        }
+   
+    
+}
+
+chat_box1.addEventListener("mouseenter", changeColor1);
+chat_box2.addEventListener("mouseenter", changeColor2);
+chat_box3.addEventListener("mouseenter", changeColor3);
+
+
+chat_box1.addEventListener("mouseleave", changeColor4);
+chat_box2.addEventListener("mouseleave", changeColor5);
+chat_box3.addEventListener("mouseleave", changeColor6);
+
+
+chat_box1.addEventListener("click", innerText1);
+chat_box2.addEventListener("click", innerText2);
+chat_box3.addEventListener("click", innerText3);
