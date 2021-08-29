af33b7037c66f0fe1ccee10a54905e2542697762
DaehyeonKim <Daehyeonoff@gmail.com>
2021-08-29 22:23:12 +0900

    20210829

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let count = 1;
 function addclick () {
     let remain = count % 2;
     
-    if(remain = 0) {
+    if(remain == 0) {
         button.innerText="hello!";
     } else {
         button.innerText="not~!";
