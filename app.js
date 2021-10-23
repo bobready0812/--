cc338faee26b890a3de1bbc5c175fc8a969755de
DaehyeonKim <Daehyeonoff@gmail.com>
2021-10-23 23:56:03 +0900

    20211023

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
+const sci = document.querySelector(".scissors");
+const rok = document.querySelector(".rock");
+const pap = document.querySelector(".paper");
+const iCh = document.querySelector(".iChoose");
+const cCh = document.querySelector(".comChoose");
+const right = document.querySelector(".right");
 
-const h1 = document.querySelector("h1");
 
-function getTime() {
-    const date = new Date();
-    const hours = String(date.getHours());
-    const minutes= String(date.getMinutes());
-
-    h1.innerText = `${hours}:${minutes}`;
+function handleClick(e) {
+ if(e.target.innerText === "가위") {
+     iCh.innerText = "가위";
+     machineChoose(0);
+ } else if (e.target.innerText === "바위"){
+     iCh.innerText = "바위";
+     machineChoose(1);
+ } else {
+     iCh.innerText = "보";
+     machineChoose(2);
+ }
 }
 
+function machineChoose (a) {
+    const ran  = Math.floor(Math.random() * 3);
+    if (ran == 0) {
+      cCh.innerText="컴퓨터가 고른것: 가위";
+    } else if (ran == 1) {
+        cCh.innerText = "컴퓨터가 고른것: 바위";
+    } else {
+        cCh.innerText = "컴퓨터가 고른것: 보";
+    }
+ 
+   if (a == 0 && ran == 0) {
+       right.innerText ="비김!";
+   } else if (a == 0 && ran == 1) {
+       right.innerText = "짐!";
+   } else if (a == 0 && ran == 2) {
+       right.innerText = "이김!";
+   } else if (a == 1 && ran == 0) {
+       right.innerText = "이김!" ;
+   } else if (a == 1 && ran == 1) {
+       right.innerText = "비김!";
+   } else if (a == 1 && ran == 2) {
+       right.innerText = "짐!" ;
+   } else if (a == 2 && ran == 0) {
+    right.innerText = "짐!" ;
+   } else if (a == 2 && ran == 1) {
+    right.innerText = "이김!";
+} else {
+    right.innerText = "비김!" ;
+} 
+    
+}
 
-
-
-
-
-
-setInterval( getTime , 1000 );
+sci.addEventListener("click", handleClick);
+rok.addEventListener("click", handleClick);
+pap.addEventListener("click", handleClick);
